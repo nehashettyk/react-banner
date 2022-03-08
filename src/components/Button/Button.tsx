6ec4fe3e5e5cee9938c23bedcec5a02774e4f70f
Srilatha <Srilatha@apples-MacBook-Pro.local>
2022-03-08 18:25:40 +0530

    added extra buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {View} from "react-native";
+import {Button} from "react-native-elements";
 
 export interface ButtonProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Buttons = (props: ButtonProps) => {
+  return (
+    <View>
+      <Button>Dismiss</Button>
+    </View>
+  );
 };
 
-export default Button;
+export default Buttons;
